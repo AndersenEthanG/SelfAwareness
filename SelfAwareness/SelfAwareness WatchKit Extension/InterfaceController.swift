b319183f4c68cdab5b73e718f0976ad796b5d545
Ethan Andersen <AndersenEthanG@gmail.com>
2021-06-03 09:36:47 -0600

    Initialization

---
+++ SelfAwareness/SelfAwareness WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,26 @@
+//
+//  InterfaceController.swift
+//  SelfAwareness WatchKit Extension
+//
+//  Created by Ethan Andersen on 6/3/21.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+    }
+
+}
